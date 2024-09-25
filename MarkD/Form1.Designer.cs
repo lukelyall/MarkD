609f49f696fc75c8ef918260a73775b35328826f
lukelyall <lukelyall2012@gmail.com>
2024-09-25 11:21:45 -0800

    updated design

--- MarkD/Form1.Designer.cs
+++ MarkD/Form1.Designer.cs
@@ -28,70 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnPanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.renderedMD = new System.Windows.Forms.WebBrowser();
-            this.saveBtn = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.notesPanel = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.Label();
+            this.saveAllBtn = new System.Windows.Forms.Button();
             this.newNoteBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.btnPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnPanel
+            // 
+            this.btnPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(42)))), ((int)(((byte)(19)))));
+            this.btnPanel.Controls.Add(this.saveAllBtn);
+            this.btnPanel.Controls.Add(this.panel3);
+            this.btnPanel.Controls.Add(this.newNoteBtn);
+            this.btnPanel.Controls.Add(this.saveBtn);
+            this.btnPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPanel.Location = new System.Drawing.Point(0, 0);
+            this.btnPanel.Name = "btnPanel";
+            this.btnPanel.Size = new System.Drawing.Size(200, 450);
+            this.btnPanel.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(23, 46);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(151, 2);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(119)))), ((int)(((byte)(45)))));
+            this.panel2.Controls.Add(this.title);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 30);
+            this.panel2.TabIndex = 7;
             // 
             // renderedMD
             // 
-            this.renderedMD.Location = new System.Drawing.Point(464, 51);
+            this.renderedMD.Location = new System.Drawing.Point(507, 29);
             this.renderedMD.MinimumSize = new System.Drawing.Size(20, 20);
             this.renderedMD.Name = "renderedMD";
-            this.renderedMD.Size = new System.Drawing.Size(329, 387);
-            this.renderedMD.TabIndex = 2;
+            this.renderedMD.Size = new System.Drawing.Size(293, 421);
+            this.renderedMD.TabIndex = 9;
             // 
-            // saveBtn
+            // notesPanel
             // 
-            this.saveBtn.Location = new System.Drawing.Point(12, 0);
-            this.saveBtn.Name = "saveBtn";
-            this.saveBtn.Size = new System.Drawing.Size(111, 23);
-            this.saveBtn.TabIndex = 3;
-            this.saveBtn.Text = "Save";
-            this.saveBtn.UseVisualStyleBackColor = true;
-            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            this.notesPanel.Location = new System.Drawing.Point(200, 29);
+            this.notesPanel.Name = "notesPanel";
+            this.notesPanel.Size = new System.Drawing.Size(312, 421);
+            this.notesPanel.TabIndex = 10;
             // 
-            // tabControl1
+            // title
             // 
-            this.tabControl1.Location = new System.Drawing.Point(12, 29);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(446, 409);
-            this.tabControl1.TabIndex = 4;
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.Control;
+            this.title.Location = new System.Drawing.Point(6, 4);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(71, 22);
+            this.title.TabIndex = 0;
+            this.title.Text = "MarkD";
+            // 
+            // saveAllBtn
+            // 
+            this.saveAllBtn.BackgroundImage = global::MarkD.Properties.Resources.save_all_svgrepo_com;
+            this.saveAllBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.saveAllBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveAllBtn.Location = new System.Drawing.Point(82, 12);
+            this.saveAllBtn.Name = "saveAllBtn";
+            this.saveAllBtn.Size = new System.Drawing.Size(20, 20);
+            this.saveAllBtn.TabIndex = 0;
+            this.saveAllBtn.UseVisualStyleBackColor = true;
+            this.saveAllBtn.Click += new System.EventHandler(this.saveAllBtn_Click);
             // 
             // newNoteBtn
             // 
-            this.newNoteBtn.Location = new System.Drawing.Point(129, 0);
+            this.newNoteBtn.BackgroundImage = global::MarkD.Properties.Resources.add_square_svgrepo_com_1_;
+            this.newNoteBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.newNoteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newNoteBtn.Location = new System.Drawing.Point(38, 12);
             this.newNoteBtn.Name = "newNoteBtn";
-            this.newNoteBtn.Size = new System.Drawing.Size(111, 23);
+            this.newNoteBtn.Size = new System.Drawing.Size(20, 20);
             this.newNoteBtn.TabIndex = 5;
-            this.newNoteBtn.Text = "New Note";
             this.newNoteBtn.UseVisualStyleBackColor = true;
             this.newNoteBtn.Click += new System.EventHandler(this.newNoteBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.BackgroundImage = global::MarkD.Properties.Resources.save_svgrepo_com_1_;
+            this.saveBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.saveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveBtn.Location = new System.Drawing.Point(130, 12);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(20, 20);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
             // 
             // MarkD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(126)))), ((int)(((byte)(82)))));
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.newNoteBtn);
-            this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.notesPanel);
             this.Controls.Add(this.renderedMD);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MarkD";
             this.Text = "MarkD";
+            this.btnPanel.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.WebBrowser renderedMD;
         private System.Windows.Forms.Button saveBtn;
-        private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.Button newNoteBtn;
+        private System.Windows.Forms.Panel btnPanel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser renderedMD;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel notesPanel;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button saveAllBtn;
     }
 }
 
--- MarkD/Form1.cs
+++ MarkD/Form1.cs
@@ -14,70 +14,133 @@ namespace MarkD
 {
     public partial class MarkD : Form
     {
-
-        private Dictionary<string, string> notes;
+        private List<TextBox> noteTexts = new List<TextBox>();
+        private int totalNotes;
 
         public MarkD()
         {
             InitializeComponent();
-            notes = new Dictionary<string, string>();
-            InitializeTabControl();
-        }
-
-        private void InitializeTabControl()
-        {
-            CreateNewNoteTab("New Note");
-        }
-
-        private void CreateNewNoteTab(string title)
-        {
-            TabPage newTab = new TabPage(title);
-            TextBox noteTextBox = new TextBox
-            {
-                Multiline = true,
-                Dock = DockStyle.Fill,
-                ScrollBars = ScrollBars.Vertical
-            };
-            noteTextBox.TextChanged += Note_TextChanged;
-            newTab.Controls.Add(noteTextBox);
-            tabControl1.TabPages.Add(newTab);
-            notes.Add(title, string.Empty);
-        }
-
-        private void Note_TextChanged(object sender, EventArgs e)
-        {
-            var noteTextBox = sender as TextBox;
-            if (noteTextBox != null)
-            {
-                string unrenderedMD = noteTextBox.Text;
-                string html = Markdown.ToHtml(unrenderedMD);
-
-                string tabTitle = tabControl1.SelectedTab.Text;
-                notes[tabTitle] = unrenderedMD;
-                renderedMD.DocumentText = html;
-            }
-        }
-
-        private void saveBtn_Click(object sender, EventArgs e)
-        {
-            using (var folderDialog = new FolderBrowserDialog())
-            {
-                if (folderDialog.ShowDialog() == DialogResult.OK)
-                {
-                    foreach (var note in notes)
-                    {
-                        string filePath = Path.Combine(folderDialog.SelectedPath, note.Key + ".md");
-                        File.WriteAllText(filePath, note.Value);
-                    }
-                    MessageBox.Show($"All notes saved in {folderDialog.SelectedPath}!", "Save Complete", MessageBoxButtons.OK);
-                }
-            }
+            totalNotes = 1;
         }
 
         private void newNoteBtn_Click(object sender, EventArgs e)
         {
-            string noteTitle = "New Note " + (tabControl1.TabCount + 1);
-            CreateNewNoteTab(noteTitle);
+            if (totalNotes <= 5)
+            {
+                Button newNoteBtn = new Button
+                {
+                    Text = "Note " + totalNotes,
+                    Name = "noteBtn" + totalNotes,
+                    ForeColor = Color.White,
+                    FlatStyle = FlatStyle.Flat,
+                    Width = 112,
+                    Height = 23
+                };
+
+                TextBox newNoteText = new TextBox
+                {
+                    Multiline = true,
+                    Visible = false,
+                    BackColor = Color.FromArgb(250, 254, 224),
+                    ScrollBars = ScrollBars.Vertical,
+                    Width = 308,
+                    Height = 421
+                };
+
+                notesPanel.Controls.Add(newNoteText);
+                newNoteText.Location = new Point(0, 0);
+                noteTexts.Add(newNoteText);
+
+                newNoteBtn.Click += (s, args) =>
+                {
+                    SwitchToNote(newNoteText);
+                };
+
+                newNoteText.TextChanged += (s, args) =>
+                {
+                    renderMarkdown(newNoteText.Text);
+                };
+
+                btnPanel.Controls.Add(newNoteBtn);
+                newNoteBtn.Location = new Point(38, 34 + (totalNotes * 34));
+
+                totalNotes++;
+
+                if (totalNotes == 2)
+                {
+                    newNoteText.Visible = true;
+                }
+            }
+        }
+
+        private void SwitchToNote(TextBox selectedNote)
+        {
+            foreach (var noteTextBox in noteTexts)
+            {
+                noteTextBox.Visible = false;
+                noteTextBox.Enabled = false;
+            }
+
+            selectedNote.Visible = true;
+            selectedNote.Enabled = true;
+            selectedNote.Focus();
+
+            renderMarkdown(selectedNote.Text);
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Markdown|*.md";
+            saveFileDialog1.Title = "Save File";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                StreamWriter sw = new StreamWriter(fs);
+
+                var visibleNote = noteTexts.Find(n => n.Visible);
+                if (visibleNote != null)
+                {
+                    sw.Write(visibleNote.Text);
+                }
+
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("File saved!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void renderMarkdown(string selectedText)
+        {
+            var html = Markdown.ToHtml(selectedText);
+            renderedMD.DocumentText = html;
+        }
+
+        private void saveAllBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Filter = "Markdown|*.md",
+                Title = "Save All Notes"
+            };
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                using (FileStream fs = (FileStream)saveFileDialog1.OpenFile())
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    foreach (var noteTextBox in noteTexts)
+                    {
+                        sw.WriteLine(noteTextBox.Text);
+                        sw.WriteLine();
+                    }
+                }
+
+                MessageBox.Show("All notes saved!", "Save All", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
